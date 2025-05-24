1c5fe7387fbb0caa4ea850d16637f3df9222cc05
Disha S K <dishask@Dishas-MacBook-Air.local>
2025-05-24 11:57:40 +0530

    lonest substring using hashset and sliding window

---
+++ DSA/SlidingWindow/LongestSubStringWithoutRepeating.java
@@ -0,0 +1,26 @@
+package DSA.SlidingWindow;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class LongestSubStringWithoutRepeating {
+    public int lengthOfLongestSubstring(String s) {
+
+        int res = 0;
+        Set<Character> set = new HashSet<>();
+        int start = 0;
+        int end = 0;
+        while (end < s.length()) {
+            char current = s.charAt(end);
+            if (!set.contains(s.charAt(end))) {
+                set.add(current);
+                res = Math.max(res, end - start + 1);
+                end++;
+            } else {
+                set.remove(s.charAt(start));
+                start++;
+            }
+        }
+        return res;
+    }
+}
